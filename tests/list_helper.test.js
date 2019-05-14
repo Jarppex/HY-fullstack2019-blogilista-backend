a014b8805318c7729fcf71ddcfc578669a5cac84
Janne Mäkelä <jarppe.makela@gmail.com>
2019-05-14 22:01:32 +0300

    4.5*: apufunktioita ja yksikkötestejä, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,3 +1,4 @@
+const testData = require('../utils/test_blog_data')
 const listHelper = require('../utils/list_helper')
 
 test('dummy returns one', () => {
@@ -5,4 +6,40 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes(testData.listWithNoBlogs)
+        expect(result).toBe(0)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+      const result = listHelper.totalLikes(testData.listWithOneBlog)
+      expect(result).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.totalLikes(testData.blogs)
+        expect(result).toBe(36)
+    })
+})
+
+describe('favorite blog', () => {
+
+    test('of empty list is undefined', () => {
+        const result = listHelper.favoriteBlog(testData.listWithNoBlogs)
+        expect(result).toEqual(undefined)
+    })
+
+    test('when list has only one blog is that one', () => {
+      const result = listHelper.favoriteBlog(testData.listWithOneBlog)
+      expect(result).toEqual(testData.listWithOneBlog[0])
+    })
+
+    test('of a bigger list is one with most likes', () => {
+        const result = listHelper.favoriteBlog(testData.blogs)
+        expect(result).toEqual(testData.blogs[2])
+    })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,27 @@
 const dummy = (blogs) => {
     return 1
-  }
+}
+
+const totalLikes = (blogs) => {
+
+    if (blogs.length === 0) {
+        return 0
+    }
+    return blogs.reduce(((allLikes, blog) => allLikes + blog.likes), 0)
+}
+
+const favoriteBlog = (blogs) => {
+
+    if (blogs.length === 0) {
+        return undefined
+    }
+    return blogs.reduce((previous, current) => {
+        return previous.likes > current.likes ? previous : current
+    })
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
