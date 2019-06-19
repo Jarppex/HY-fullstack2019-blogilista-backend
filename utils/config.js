8901c46f70700d5995086d42dad8e4ec4f3a173c
Janne Mäkelä <jarppe.makela@gmail.com>
2019-06-19 16:12:26 +0300

    new testRouter added

--- utils/config.js
+++ utils/config.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+let NODE_ENV = process.env.NODE_ENV
 
-if (process.env.NODE_ENV === 'test') {
+if (NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
 }
 
 let SECRET = process.env.SECRET
 
 module.exports = {
+  NODE_ENV,
   MONGODB_URI,
   PORT,
   SECRET
